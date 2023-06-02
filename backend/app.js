805bbef409304131a4a749333bb87e0ec00d35cc
Harsh528123 <hmkothar@ualberta.ca>
2023-06-01 20:31:22 -0600

    did basic event graphql query and mutation

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ function where express expects a middleware function
 takes incoming req and funnel through graphql parser
 and automatically forward them to right resolvers
 */
-
+const events = []
 const { buildSchema } = require('graphql') // pulls properties from exported object
 const app = express();
 
@@ -17,29 +17,51 @@ app.use(bodyParser.json());
 // express-graphql allows us to point at schemas and resolvers
 // graphql will allow us to define this schema
 
-
+// exclamatoin means not nullable
 // first exclamation point means it will always return a list of strings. Can be empty but not null
 // second exclamation means it always returns a list not null.
 app.use('/graphql', graphqlHTTP({
+    // _id for mongo
     schema: buildSchema(`
+    type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+    input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
     type RootQuery {
-        events : [String!]!
+        events : [Event!]!
     }
     type RootMutation {
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
     }
     schema {
         query: RootQuery
         mutation: RootMutation
     }
     `),
+    // input type can be used in arguments
     rootValue: {
         events: () => {
-            return ['Test', 'Test1', 'Test2']
+            return events
         },
         createEvent : (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+            }
+            events.push(event)
+            return event
         }
         // a js object that has all resolvers in it
         // resolver functions need to match our schema endpoints by name
