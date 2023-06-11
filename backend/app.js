b8d9c9fbd49e6cfbc3fee1af98c364188a0c3a8c
Harsh528123 <hmkothar@ualberta.ca>
2023-06-11 13:10:17 -0600

    added user schema in mongo

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {graphqlHTTP} = require('express-graphql')
+
+const bcrypt = require('bcryptjs');
 /* 
 function where express expects a middleware function
 takes incoming req and funnel through graphql parser
 and automatically forward them to right resolvers
 */
 const mongoose = require('mongoose')
-const Event = require('./models/event')
+const Event = require('./models/event');
+const User = require('./models/user');
 const { buildSchema } = require('graphql') // pulls properties from exported object
 const app = express();
 
@@ -22,8 +25,10 @@ app.use(bodyParser.json());
 // first exclamation point means it will always return a list of strings. Can be empty but not null
 // second exclamation means it always returns a list not null.
 app.use('/graphql', graphqlHTTP({
-    // _id for mongo
+    // _id for mongo; below is the graphql schema
+    // we dont want password returned so we dont put exclamation on it
     schema: buildSchema(`
+
     type Event {
         _id: ID!
         title: String!
@@ -31,18 +36,34 @@ app.use('/graphql', graphqlHTTP({
         price: Float!
         date: String!
     }
+
+    type User {
+        _id: ID!
+        email: String!
+        password: String
+    }
+
     input EventInput {
         title: String!
         description: String!
         price: Float!
         date: String!
     }
+
+    input UserInput {
+        email: String!
+        password: String!
+    }
+
     type RootQuery {
         events : [Event!]!
     }
+
     type RootMutation {
         createEvent(eventInput: EventInput): Event
+        createUser(userInput: UserInput): User
     }
+
     schema {
         query: RootQuery
         mutation: RootMutation
@@ -59,7 +80,7 @@ app.use('/graphql', graphqlHTTP({
                 throw err;
             })
         },
-        createEvent : args => {
+        createEvent: args => {
             // const event = {
             //     _id: Math.random().toString(),
             //     title: args.eventInput.title,
@@ -71,22 +92,61 @@ app.use('/graphql', graphqlHTTP({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: new Date(args.eventInput.date)
+                date: new Date(args.eventInput.date),
+                creator: '6484a17aabc8c14166085882'
             });
+            let createdEvent; 
             return event
                 .save()
                 .then(result => {
-                    console.log(result);
-                    return {...result._doc};
-                }).catch(err => {
+                    createdEvent = { ...result._doc, _id: result._doc._id.toString() }
+                    return User.findById('6484a17aabc8c14166085882')
+                })
+                .then(user => {
+                    if (!user) {
+                        throw new Error('User not found.');
+                    }
+                    user.createdEvents.push(event);
+                    return user.save();
+                })
+                .then(result => {
+                    return createdEvent;
+                })
+                .catch(err => {
                     console.log(err);
                     throw err;
                 });
-            return event;
-        }
+        },
+        createUser: args => {
+            return User.findOne({email: args.userInput.email}).then(
+                user => {
+                    // will always end up in the then block unless we have a correction error
+                    if (user) {
+                        throw new Error('User exists already.')
+                    } else {
+                        return bcrypt.hash(args.userInput.password, 12)
+                        .then(hashedPassword => {
+                            const user = new User({
+                                email: args.userInput.email,
+                                password: hashedPassword,
+                            });
+                            return user.save()
+                            }).then(result => {
+                                return {...result._doc, _id: result.id, password: null};
+                            }).catch(err => {
+                                console.log(err);
+                                throw err;
+                            })
+                        .catch(err => {
+                            throw err;
+                        })
+                    }
+                }
+            )}
         // a js object that has all resolvers in it
         // resolver functions need to match our schema endpoints by name
     },
+
     graphiql:true
 }));
 // RootQuery will support real endpoints for the queries
---
+++ backend/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdEvents: [
+        {
+           type: Schema.Types.ObjectId,
+           ref: 'Event'
+           // allows us to set up a relationship b/w two models
+        }
+    ]
+});
+
+/*
+Two ways users are connected to events:
+user creates an event
+user books an event
+*/
+
+module.exports = mongoose.model('User', userSchema)
